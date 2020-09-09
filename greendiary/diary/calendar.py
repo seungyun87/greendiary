291ada9a47a3bd00c7e60a1f1d5bf2ea398fb589
seungyun87 <calebads@likelion.org>
2020-09-09 18:14:10 +0900

    달력경로

--- greendiary/diary/calendar.py
+++ greendiary/diary/calendar.py
@@ -15,7 +15,7 @@ class Calendar(HTMLCalendar):
 		d = ''
 		#여기서 이미지 출력.
 		for event in events_per_day:
-			d += f'<li><a href="#"><img class="thumnail" src="{event.image.url}" width="50" height="50"></a></li>'
+			d += f'<li><a href="../list"><img class="thumnail" src="{event.image.url}" width="50" height="50"></a></li>'
 
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul>{d}</ul></td>"
